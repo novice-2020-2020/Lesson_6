386dfe5d9a187c04063a9c580aa85281f5253669
sm <43108168+novice-2020-2020@users.noreply.github.com>
2020-09-01 22:16:03 +0530

    Update 21.js
    Changes to the game play and initial turn, fixes with eslint

--- 21.js
+++ 21.js
@@ -1,20 +1,22 @@
 const readline = require('readline-sync');
 const cards = {
-'2': 2, 
-'3': 3, 
-'4': 4, 
-'5': 5, 
-'6': 6, 
-'7': 7, 
-'8': 8, 
-'9': 9, 
-'10': 10, 
-'A': [1, 11], 
-'J': 10, 
-'K': 10, 
-'Q': 10
-}
-
+  2: 2,
+  3: 3,
+  4: 4,
+  5: 5,
+  6: 6,
+  7: 7,
+  8: 8,
+  9: 9,
+  10: 10,
+  A: [1, 11],
+  J: 10,
+  K: 10,
+  Q: 10
+};
+const BLACKJACK = 21;
+const DEALERLIMIT = 17;
+const VALIDHITORSTAYS = ['hit', 'h', 'stay', 's'];
 let playerCounter = 0;
 let dealerCounter = 0;
 let playerCards = [];
@@ -26,149 +28,146 @@ let currentChance = 'Player';
 function initializeDeck() {
   let deck = [];
   Object.keys(cards).forEach(card => {
-    for (let index = 0; index < 4; index ++) {
+    for (let index = 0; index < 4; index++) {
       deck.push(card);
     }
-  })
+  });
   return deck;
 }
 
-function cardSelection(deck, arr, scoreArr) {
-  let card = deck.splice(Math.floor(Math.random() * deck.length),1).toString();
-
+function dealCards(deck, arr, scoreArr) {
+  let card = deck.splice(Math.floor(Math.random() * deck.length), 1).toString();
   let score = scoreArr.reduce((accum, num) => accum + num, 0);
-
   if (score <= 10 && card === 'A') {
     arr.push(card);
     scoreArr.push(11);
-  }
-  else if (score > 10 && card === 'A') {
+  } else if (score > 10 && card === 'A') {
     arr.push(card);
     scoreArr.push(1);
-  }
-  else {
+  } else {
     arr.push(card);
     scoreArr.push(Number(cards[card]));
   }
 }
 
-function dealCards(deck, arr, scoreArr, counter) {
-  if (counter === 0) {
-    for (let i = 0; i < 2; i++) {
-      cardSelection(deck, arr, scoreArr);
-    }} 
-  else {
-    cardSelection(deck, arr, scoreArr);
+function firstMove(deck, arr, scoreArr) {
+  for (let index = 0; index < 2; index++) {
+    dealCards(deck, arr, scoreArr);
   }
 }
 
+
 function calcScore(scoreArr) {
-  return scoreArr.reduce((accum, num) => accum + num, 0)
+  return scoreArr.reduce((accum, num) => accum + num, 0);
 }
 
 function isBust(scoreArr) {
-  return scoreArr.reduce((accum, num) => accum + num, 0) > 21;
+  return scoreArr.reduce((accum, num) => accum + num, 0) > BLACKJACK;
 }
 
 function isBlackJack(scoreArr) {
-  let score = scoreArr.reduce((sum, num) => sum+num, 0);
-  return score === 21;
+  let score = scoreArr.reduce((sum, num) => sum + num, 0);
+  return score === BLACKJACK;
 }
 
-function displayInformation(cardsArr, scores) {
-  console.log(`The ${currentChance}'s deck is (${cardsArr.join(", ")}), and the score is ${scores.reduce((sum, num) => sum+num, 0)}`);
+function displayInformation(cardsArr, scores, counter, currentChance = 'Player') {
+  if (counter === 0 && currentChance === 'Player') {
+    console.log(`The Player's deck is (${cardsArr.join(", ")}) and the score is ${calcScore(scores)}`);
+  } else if (counter === 0 && currentChance === 'Dealer') {
+    console.log('The Dealer\'s deck is (' + (cardsArr[0] + ', ' + 'X)'));
+  } else {
+    console.log(`The ${currentChance}'s deck is (${cardsArr.join(", ")}) and the score is ${calcScore(scores)}`);
+  }
 }
 
 function playerHitOrStay() {
-  let decision = readline.question("Hit or Stay: ").toLowerCase().trim();
-  return decision[0] === 'h' ? 'h' : 's';
+  let decision = readline.question("\nWould you like to Hit or Stay: ").toLowerCase().trim();
+  while (true) {
+    if (VALIDHITORSTAYS.includes(decision)) {
+      if (decision[0] === 'h' || decision[0] === 's') return decision;
+    } else {
+
+      decision = readline.question("\nNot a valid response, would you like to Hit or Stay: ").toLowerCase().trim();
+    }
+  }
 }
 
 function playerPlays(deck, playerCards, playerScores, playerCounter) {
-  dealCards(deck, playerCards, playerScores, playerCounter);
-  playerCounter+=1;
-  displayInformation(playerCards, playerScores);
-  
+  playerCounter += 1;
   while (true) {
     if (isBlackJack(playerScores)) {
       displayInformation(playerCards, playerScores);
       console.log(`${currentChance} hit BlackJack`);
-      break;}
-
-    else if(isBust(playerScores)) {
-      console.log(`${currentChance} is bust`);
-      // displayInformation(playerCards, playerScores);
       break;
-    }
-    else {
-      if (playerHitOrStay() === 's') {
-        displayInformation(playerCards, playerScores);
-        console.log( `you stayed`);
-        break;
-      }
-      else {
-        dealCards(deck, playerCards, playerScores, playerCounter);
-        displayInformation(playerCards, playerScores);
-      }
+    } else if (isBust(playerScores)) {
+      console.log('Player is bust');
+      break;
+    } else if (playerHitOrStay() === 's') {
+      // displayInformation(playerCards, playerScores);
+      console.log(`\nPlayer stayed the game. The Player's deck is (${playerCards.join(", ")}) and the score is ${calcScore(playerScores)}\n`);
+      break;
+    } else {
+      dealCards(deck, playerCards, playerScores, playerCounter);
+      displayInformation(playerCards, playerScores);
     }
   }
   currentChance = 'Dealer';
 }
 
 function dealerHitOrStay(deck, dealerScores) {
-  // let score = calcScore(dealerScores);
-  while (calcScore(dealerScores) <= 17) {
+  while (calcScore(dealerScores) <= DEALERLIMIT) {
     dealCards(deck, dealerCards, dealerScores, dealerCounter);
     calcScore(dealerScores);
-  } 
+  }
 }
 
 function dealerPlays(deck, dealerCards, dealerScores) {
-  dealCards(deck, dealerCards, dealerScores, dealerCounter);
-  // displayInformation(dealerCards, dealerScores);
-  dealerCounter+=1;
-  while(calcScore(dealerScores) <= 17 || !isBust(dealerScores) || !isBlackJack(dealerScores)) {
+  dealerCounter += 1;
+  while (calcScore(dealerScores) <= DEALERLIMIT || !isBust(dealerScores) || !isBlackJack(dealerScores)) {
     dealerHitOrStay(deck, dealerScores);
-    displayInformation(dealerCards, dealerScores);
-    if(isBust(dealerScores)) {
+    displayInformation(dealerCards, dealerScores, dealerCounter, 'Dealer');
+    if (isBust(dealerScores)) {
       console.log('Dealer bust');
       break;
-    }
-    else if(isBlackJack(dealerScores)) {
+    } else if (isBlackJack(dealerScores)) {
       console.log('Dealer hit BlackJack');
       break;
-    }break;}
-    
+    }
+    break;
+  }
 }
 
-function result(playerPlays, dealerPlays) {
-  if(isBust(playerScores) && !isBust(dealerScores)) {
+function result(playerScores, dealerScores) {
+  if (isBust(playerScores)) {
     return `Dealer Wins`;
-  }
-  else if(isBust(dealerScores) && !isBust(playerScores)) {
+  } else if (isBust(dealerScores) && !isBust(playerScores)) {
     return `Player Wins`;
-  }
-  else if (isBlackJack(dealerScores) && !isBlackJack(playerScores)) {
+  } else if (isBlackJack(dealerScores) && !isBlackJack(playerScores)) {
     return `Dealer Wins`;
-  }
-  else if (!isBlackJack(dealerScores) && isBlackJack(playerScores)) {
+  } else if (!isBlackJack(dealerScores) && isBlackJack(playerScores)) {
     return `Player Wins`;
-  }
-  else if (calcScore(playerScores) > calcScore(dealerScores)) {
+  } else if (calcScore(playerScores) > calcScore(dealerScores)) {
     return `Player Wins`;
-  }
-  else if (calcScore(playerScores) < calcScore(dealerScores)) {
+  } else if (calcScore(playerScores) < calcScore(dealerScores)) {
     return `Dealer Wins`;
-  }
-  else {
-    return `Tie!`
+  } else {
+    return `Tie!`;
   }
 }
+
 function playTwentyOne() {
+  console.clear();
   let deck = initializeDeck();
+  firstMove(deck, playerCards, playerScores);
+  displayInformation(playerCards, playerScores, playerCounter);
+  firstMove(deck, dealerCards, dealerScores);
+  displayInformation(dealerCards, dealerScores, dealerCounter, 'Dealer');
   playerPlays(deck, playerCards, playerScores, playerCounter);
-  dealerPlays(deck, dealerCards, dealerScores, dealerCounter);
-  console.log(result(playerPlays, dealerPlays));
+  if (isBust(playerScores)) {
+    return 'Dealer Wins!';
+  }
+  dealerPlays(deck, dealerCards, dealerScores);
+  return (result(playerScores, dealerScores));
 }
 
 playTwentyOne();
